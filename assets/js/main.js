cfe7c54a79f6f27615c3972a9a0223eebe306947
dumbcoder0101 <mramakda523@gmail.com>
2024-06-24 19:31:41 +0530

    About & Home is merged and changes in dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,3 +147,25 @@ document.addEventListener("DOMContentLoaded", () => {
   // Ensure dropdown is hidden on page load
   workDropdown.style.display = "none";
 });
+//Back to top JS
+
+// Get the button
+let mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
